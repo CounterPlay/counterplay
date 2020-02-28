0dea8aeb1e4ee28f533abae8a99545754ce28e84
BearGuy <stephen@peterkins.ca>
2020-02-28 14:18:55 +0900

    inclusion of react frontend and restructuring

---
+++ server/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useStaticAssets({
+    root: join(__dirname, '..', '..', 'public')
+  });
+  await app.listen(3000);
+}
+bootstrap();
+
